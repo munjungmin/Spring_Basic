10e3d482848b4eb155cfb43cb6af0fa86c8c88c7
munjungmin <answjdals77@gmail.com>
2024-05-06 14:37:04 +0900

    JPA를 이용한 DB 접근

--- hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,13 @@
 package hello.hellospring.domain;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;  // 일련번호 고객이 정하는 id가 아니라 시스템이 부여하는 임의의 값
+
     private String name;
 
     public Long getId() {
--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -6,10 +6,12 @@ import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional   // jpa를 사용하려면 저장하고 변경할때 트랜잭션이 항상 필요
 public class MemberService {
 
     private final MemberRepository memberRepository;
